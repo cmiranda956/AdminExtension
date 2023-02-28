da16fa04b30206550f7362dbb38f23a8d44a7f02
Cesar <cmiranda956@gmail.com>
2023-02-28 13:59:28 -0600

    debugging

--- js/header.js
+++ js/header.js
@@ -1,19 +1,20 @@
-const loc = document.getElementsByClassName('suit-status')[0];
+const loc = document.getElementsByClassName('TabMenu');
+const adminMenu = document.createElement('li');
+
+adminMenu.innerHTML = `
+    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="adminMenu">
+        <i class="fa fa-cog"></i> Admin
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="adminMenu">
+        <li>
+            <button type="button" class="dropdown-item" id="toggleHeader"> 
+                Toggle Header 
+            </button>
+        </li>
+    </ul>
+`
 loc.appendChild(adminMenu);
 
-const adminMenu = document.createElement('li');
-adminMenu.innerHTML = `
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="adminMenu">
-     <i class="fa fa-cog"></i> Admin
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="adminMenu">
-      <li>
-          <button type="button" class="dropdown-item" id="toggleHeader"> 
-            Toggle Header 
-          </button>
-      </li>
-  </ul>
-`
 document.getElementById('toggleHeader').addEventListener('click', () => {
     toggleHeader();
 });
@@ -23,8 +24,3 @@ function toggleHeader() {
     if(header.style.display == '') header.style.display = 'none';
     else header.style.display = '';
 }
-
-
-
-
-//export { toggleHeader };
