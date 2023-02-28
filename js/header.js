7e9b4b0b2d564db424887eed6d5b2955b32e7ce3
Cesar <cmiranda956@gmail.com>
2023-02-28 13:57:56 -0600

    debugging

--- js/header.js
+++ js/header.js
@@ -1,12 +1,19 @@
 const loc = document.getElementsByClassName('suit-status')[0];
-const adminMenu = document.createElement('div');
-adminMenu.innerHTML = `
-        <button type="button" class="dropdown-item" id="toggleHeader"> 
-            Toggle Header 
-        </button>
-    `
 loc.appendChild(adminMenu);
 
+const adminMenu = document.createElement('li');
+adminMenu.innerHTML = `
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="adminMenu">
+     <i class="fa fa-cog"></i> Admin
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="adminMenu">
+      <li>
+          <button type="button" class="dropdown-item" id="toggleHeader"> 
+            Toggle Header 
+          </button>
+      </li>
+  </ul>
+`
 document.getElementById('toggleHeader').addEventListener('click', () => {
     toggleHeader();
 });
@@ -16,3 +23,8 @@ function toggleHeader() {
     if(header.style.display == '') header.style.display = 'none';
     else header.style.display = '';
 }
+
+
+
+
+//export { toggleHeader };
