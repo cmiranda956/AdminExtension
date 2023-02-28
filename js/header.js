8e377023359f8e918ebe2224d4e8f9d08401fe82
Cesar <cmiranda956@gmail.com>
2023-02-28 12:19:25 -0600

    added toggle header button

--- js/header.js
+++ js/header.js
@@ -3,31 +3,22 @@ const adminMenu = document.createElement('div');
 adminMenu.classList.add('dropdown');
 adminMenu.innerHTML = `
   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="adminMenu">
-     <i class="fa fa-cog" /> Admin
+     <i class="fa fa-cog"></i> Admin
   </button>
   <ul class="dropdown-menu" aria-labelledby="adminMenu">
       <li>
-          <a class="dropdown-item" href="#">
-            <img src="https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/icons/flags/DE.png" /> Deutsch
-          </a>
-      </li>
-      <li>
-          <a class="dropdown-item" href="#">
-            <img src="https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/icons/flags/GB.png" /> English(UK)
-          </a>
-      </li>
-      <li>
-          <a class="dropdown-item" href="#">
-            <img src="https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/icons/flags/BR.png" /> Brasil
-          </a>
+          <button class="dropdown-item" id="toggleHeader"> 
+            Toggle Header 
+          </button>
       </li>
   </ul>
 `
 loc.appendChild(adminMenu);
 
-//adminMenu.addEventListener('click', () => {
-//    toggleHeader();
-//});
+document.getElementById('toggleHeader').addEventListener('click', () => {
+    toggleHeader();
+});
+
 function toggleHeader() {
     var header = document.querySelector('#header .juvare');
     if(header.style.display == '') header.style.display = 'none';
