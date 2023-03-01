796f10b73fece535879022fbf1b09eee68da5dce
Cesar <cmiranda956@gmail.com>
2023-03-01 10:48:57 -0600

    debugging

--- js/background.js
+++ js/background.js
@@ -1,6 +1,13 @@
 const requestFilter = {
     urls: ['https://*boards/board.aspx?tableid=*'],
 }
+
 chrome.webRequest.onCompleted.addListener((details) => {
-    let frameId = details.frameId
+    let frameId = details.frameId;
+    let tabId = details.tabId;
+    chrome.scripting.executeScript({
+        target: {tabId: tabId},
+        frameIds: [frameId],
+        files: ["script.js"]
+    });
 }, requestFilter);
