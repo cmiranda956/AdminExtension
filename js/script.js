b592f85495a4860aef2287302d1fba0d196ddc69
Cesar <cmiranda956@gmail.com>
2023-03-01 16:21:08 -0600

    debugging

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-console.log('we are in the script.js');
-(function() {let menu = document.getElementById('adminMenu');
-menu.classList.add('dropdown');
-menu.innerHTML = `
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="adminMenu" data-toggle="dropdown" aria-haspopup="true" area-expanded="false">
+(function() {
+    const menu = document.getElementById('adminMenu');
+    if(!menu) {
+        console.log('adminMenu not found');
+        return;
+    }
+    menu.classList.add('dropdown');
+    menu.innerHTML = `
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="adminMenu" data-toggle="dropdown" aria-haspopup="true" area-expanded="false">
         <i class="material-icons">admin_panel_settings</i> Admin
-    </button>
-    <div class="dropdown-menu" aria-labelledby="adminMenu">
+        </button>
+        <div class="dropdown-menu" aria-labelledby="adminMenu">
         <a class="dropdown-item" id="toggleHeader">Toggle Header </a>
-    </div>
-    `
+        </div>
+        `
 })();
 
 document.getElementById('toggleHeader').addEventListener('click', () => {
