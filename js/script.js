2f835d757cf5325cd87652639c69a1a8ae2adccf
Cesar <cmiranda956@gmail.com>
2023-03-02 09:28:19 -0600

    debugging

--- js/script.js
+++ js/script.js
@@ -15,12 +15,12 @@
         `
 })();
 
-//document.getElementById('toggleHeader').addEventListener('click', () => {
-//   toggleHeader();
-//});
-
 function toggleHeader() {
     var header = document.querySelector('#header .juvare');
     if(header.style.display == '') header.style.display = 'none';
     else header.style.display = '';
 }
+
+document.getElementById('toggleHeader').addEventListener('click', () => {
+   toggleHeader();
+});
