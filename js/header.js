3466c0d40452ec29cf8e50cc08e1f6fc2f84e81d
Cesar <cmiranda956@gmail.com>
2023-03-02 11:18:40 -0600

    debugging

--- js/header.js
+++ js/header.js
@@ -1,9 +1,12 @@
-document.getElementById('toggleHeader').addEventListener('click', () => {
-    toggleHeader();
-});
+(function() {
+    document.getElementById('toggleHeader').addEventListener('click', () => {
+        toggleHeader();
+    });
 
-function toggleHeader() {
-    const header = window.top.document.querySelector('#header > .juvare');
-    if(header.style.display == '') header.style.display = 'none';
-    else header.style.display = '';
-}
+    function toggleHeader() {
+        console.log('at toggleHeader function');
+        const header = window.top.document.querySelector('#header > .juvare');
+        if(header.style.display == '') header.style.display = 'none';
+        else header.style.display = '';
+    }
+})();
