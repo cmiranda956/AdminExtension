5a05664dbfe0d040bc75f92fe294dff7c6f8732e
Cesar <cmiranda956@gmail.com>
2023-03-02 09:39:16 -0600

    debugging

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 const requestFilter = {
-    urls: ['https://192.168.100.100/eoc9/boards//board.aspx*'],
+    urls: ['https://192.168.100.100/eoc9/boards/board.aspx?tableid=*'],
 }
 
 chrome.webRequest.onCompleted.addListener((details) => {
