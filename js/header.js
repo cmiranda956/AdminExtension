55c1290c8feae14bafa7a4c64d9ebba84c8bbed5
Cesar <cmiranda956@gmail.com>
2023-03-02 11:16:01 -0600

    debugging

--- js/header.js
+++ js/header.js
@@ -1,3 +1,7 @@
+document.getElementById('toggleHeader').addEventListener('click', () => {
+    toggleHeader();
+});
+
 function toggleHeader() {
     const header = window.top.document.querySelector('#header > .juvare');
     if(header.style.display == '') header.style.display = 'none';
